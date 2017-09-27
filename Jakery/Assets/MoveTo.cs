4fcb8c800c67d48b554b739b5cf102ba00fbffec
Jordan O'Leary <jdoleary@gmail.com>
2017-09-26 22:56:00 -0400

    sizing

---
+++ Jakery/Assets/MoveTo.cs
@@ -0,0 +1,15 @@
+﻿ using UnityEngine;
+    using System.Collections;
+    
+    public class MoveTo : MonoBehaviour {
+       
+       public Transform goal;
+       public UnityEngine.AI.NavMeshAgent agent;
+       
+       void Start () {
+          agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+       }
+       void Update () {
+          agent.destination = goal.position; 
+       }
+    }

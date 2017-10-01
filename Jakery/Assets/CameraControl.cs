1ecdab603344ddc1bd63912e94bac7ecb752fd4d
Jordan O'Leary <jdoleary@gmail.com>
2017-10-01 12:48:15 -0400

    feat (nav) better navemesh and movement.

--- Jakery/Assets/CameraControl.cs
+++ Jakery/Assets/CameraControl.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
  public Transform around;
+ public Transform followTarget;
+ public float speed = 5f;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,12 @@ public class CameraControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.RotateAround(around.position, Vector3.up, 20 * Time.deltaTime);
-	}
+		//transform.RotateAround(around.position, Vector3.up, 20 * Time.deltaTime);
+    float step = speed * Time.deltaTime;
+    transform.position = Vector3.MoveTowards(transform.position, followTarget.position, step);
+    if (Input.GetKeyDown("up")){
+      transform.RotateAround(around.position, Vector3.up, 90);
+    }
+
+  }
 }

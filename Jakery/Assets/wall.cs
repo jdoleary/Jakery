e2644925c2559382ab43c4ea0c3322f2943f2bf1
Jordan O'Leary <jdoleary@gmail.com>
2017-10-01 18:33:10 -0400

    feat (walls) walls fade

--- Jakery/Assets/wall.cs
+++ Jakery/Assets/wall.cs
@@ -6,22 +6,39 @@ public class wall : MonoBehaviour {
   private Transform cam;
   private Transform player;
   private Renderer rend;
+  
+  private Material opaque;
+  private Material faded;
   public bool isSmall;
+  private bool wasHidden;
+  private bool hide;
 	// Use this for initialization
 	void Start () {
 		cam = Camera.main.gameObject.GetComponent<Transform>();
     player = GameObject.FindWithTag("Player").GetComponent<Transform>();
     rend = GetComponent<Renderer>();
+    opaque = Resources.Load("Materials/Wall") as Material;
+    faded = Resources.Load("Materials/Wall Faded") as Material;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-    rend.enabled = true;
+    hide = false;
     if(isSmall && ((cam.position.z < transform.position.z && transform.position.z < player.position.z) || (player.position.z < transform.position.z && transform.position.z < cam.position.z))){
-      rend.enabled = false;
+      hide = true;
     }
     if(!isSmall && ((cam.position.x < transform.position.x && transform.position.x < player.position.x) || (player.position.x < transform.position.x && transform.position.x < cam.position.x))){
-      rend.enabled = false;
+      hide = true;
+    }
+    if(hide){
+      rend.material = faded; 
+      wasHidden = true;
+    }else{
+      if(wasHidden){
+        rend.material = opaque; 
+        wasHidden = false;
+      }
+      
     }
 	}
 }

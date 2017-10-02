eed3bc4ed9300accaf3edb513b91727af258b368
Jordan O'Leary <jdoleary@gmail.com>
2017-10-01 20:51:11 -0400

    feat (Movement) Click to move

---
+++ Jakery/Assets/Scripts/MoveToClickPoint.cs
@@ -0,0 +1,28 @@
+﻿ // MoveToClickPoint.cs
+    using UnityEngine;
+    
+    public class MoveToClickPoint : MonoBehaviour {
+        UnityEngine.AI.NavMeshAgent agent;
+        
+        void Start() {
+            agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        }
+        
+        void Update() {
+          // Raycast to click
+          if (Input.GetMouseButtonDown(0)) {
+              Debug.Log("mouse down");
+              RaycastHit hit;
+              
+              if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)) {
+                  Debug.Log("hit " + hit.transform.gameObject);
+                  agent.destination = hit.point;
+              }
+          }
+          // When unit arrives at destination:
+          if(agent.remainingDistance <= float.Epsilon)
+          {
+            Debug.Log("Arrived");
+          }
+        }
+    }

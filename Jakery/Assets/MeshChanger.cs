c0fab6acb3700793327af9c75af7676736afef89
Jordan O'Leary <jdoleary@gmail.com>
2017-10-03 20:45:20 -0400

    feat(character)animation

---
+++ Jakery/Assets/MeshChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class MeshChanger : MonoBehaviour {
+	public float fps;
+	public Mesh[] meshes;
+
+	MeshFilter meshFilter;
+
+	void Awake() {
+		meshFilter = GetComponent<MeshFilter> ();
+	}
+
+	void Update () {
+		int index = ((int)(Time.time * fps)) % meshes.Length;
+		meshFilter.sharedMesh = meshes [index];
+	}
+}
--- Jakery/Assets/Scripts/MoveToClickPoint.cs
+++ Jakery/Assets/Scripts/MoveToClickPoint.cs
@@ -3,12 +3,22 @@
     
     public class MoveToClickPoint : MonoBehaviour {
         UnityEngine.AI.NavMeshAgent agent;
-        
+	public GameObject Anim_Standing;
+	public GameObject Anim_Walking;
         void Start() {
             agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		Anim_Standing.SetActive (true);
+		Anim_Walking.SetActive (false);
         }
         
         void Update() {
+		// When unit arrives at destination:
+		if(agent.remainingDistance <= float.Epsilon)
+		{
+			Anim_Standing.SetActive (true);
+			Anim_Walking.SetActive (false);
+			Debug.Log("Arrived");
+		}
           // Raycast to click
           if (Input.GetMouseButtonDown(0)) {
               Debug.Log("mouse down");
@@ -17,12 +27,10 @@
               if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)) {
                   Debug.Log("hit " + hit.transform.gameObject);
                   agent.destination = hit.point;
+				Anim_Standing.SetActive (false);
+				Anim_Walking.SetActive (true);
               }
           }
-          // When unit arrives at destination:
-          if(agent.remainingDistance <= float.Epsilon)
-          {
-            //Debug.Log("Arrived");
-          }
+        
         }
     }
